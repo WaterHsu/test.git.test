4cd44130c23922831b50c108ec800ca140318b60
WaterHsu <hsupangfei1989@gmail.com>
2013-04-06 18:27:44 +0800

    测试Eclipse上的git
    测试Eclipse上的git！

---
+++ testgit/testgit/src/com/Testgit.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Testgit 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! from Eclipse!");
+	}
+
+}
