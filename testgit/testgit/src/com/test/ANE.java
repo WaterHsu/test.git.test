723ece9d401a2748c794805b032d89c16fc71a78
WaterHsu <WaterHsu@WaterHsu-PC>
2014-08-20 17:13:34 +0800

    111

---
+++ testgit/testgit/src/com/test/ANE.java
@@ -0,0 +1,16 @@
+/**
+ * 秀吧网络科技有限公司版权所有
+ * Copyright (C) xiu8 Corporation. All Rights Reserved
+ */
+package com.test;
+
+/**
+ * @author WaterHsu@xiu8.com
+ * @version 2014年8月20日
+ */
+public class ANE {
+	public static void main(String[] args)
+	{
+		System.out.println("FFFF");
+	}
+}
