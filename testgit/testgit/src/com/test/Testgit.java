777446645212b0e7139ffe86004aa8246a370a15
WaterHsu <WaterHsu@WaterHsu-PC>
2014-08-20 17:04:40 +0800

    ff

---
+++ testgit/testgit/src/com/test/Testgit.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Testgit 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! from Eclipse!");
+		System.out.println("Test the git in eclipse!");
+		System.out.println("Hello World");
+	}
+
+}
